b5e974a0dde1943d5fcca9c254b3b27fa0cc2e5a
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-08-25 17:39:01 -0400

    weiToEther

--- internal/server/config.go
+++ internal/server/config.go
@@ -4,17 +4,18 @@ type Config struct {
 	network    string
 	httpPort   int
 	interval   int
-	payout     int
+	payout     *big.Int
 	proxyCount int
 	queueCap   int
 }
 
 func NewConfig(network string, httpPort, interval, payout, proxyCount, queueCap int) *Config {
+	ether := new(big.Int).Exp(big.NewInt(10), big.NewInt(18), nil)
 	return &Config{
 		network:    network,
 		httpPort:   httpPort,
 		interval:   interval,
-		payout:     payout,
+		payout:     new(big.Int).Mul(big.NewInt(int64(payout)), ether),
 		proxyCount: proxyCount,
 		queueCap:   queueCap,
 	}
